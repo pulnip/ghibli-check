c8e15f02b1cdfcdab10299f5254b88784b40d337
pulnip <choiwh1008@gmail.com>
2025-06-05 22:33:31 +0900

    improve ResNet learning script

--- resnet_learn.py
+++ resnet_learn.py
@@ -4,9 +4,10 @@ import torch.optim as optim
 from torchinfo import summary
 from argparse import ArgumentParser
 import re
+import os
 
 from my_data import get_dataloaders, visualize_random_batches
-from model import resnet
+from model import resnet, pretrained_resnet
 from train import classifier_one_epoch, train, report_train_result
 from callbacks import EarlyStopping
 from my_util import DEVICE
@@ -23,22 +24,35 @@ if __name__ == '__main__':
     match = re.search(r"resnet(\d+)", MODEL_NAME)
     MODEL_SIZE = int(match.group(1)) if match else 18
 
-    loaders = get_dataloaders()
+    BATCH_SIZE = 128
+    DATASET_SIZE = 10_000
+    NUM_WORKERS = os.cpu_count()
+
+    loaders = get_dataloaders(num_data=DATASET_SIZE,
+                              batch_size=BATCH_SIZE,
+                              num_workers=NUM_WORKERS)
     # visualize_random_batches(loaders[0], num_batches=5)
 
     model = resnet(MODEL_SIZE, num_classes=2).to(DEVICE)
+    # model = pretrained_resnet(18, num_classes=2).to(DEVICE)
     model_info = summary(model, verbose=0)
     with open(f"{MODEL_NAME}_summary.txt", "w") as f:
         f.write(str(model_info))
-    # 1. Using Cross Entropy
+    # 1. Using Cross Entropys
     criterion = nn.CrossEntropyLoss()
     # 2. Using Binary Cross Entropy
     # criterion = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
-    es = EarlyStopping(patience=5, restore_best_weights=True)
+    optimizer = optim.SGD(model.parameters(), lr=0.1,
+                          momentum=0.9, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer=optimizer, milestones=[30, 60, 90],
+        gamma=0.1
+    )
+    es = EarlyStopping(patience=10, restore_best_weights=True)
     result = train(model, loaders, criterion, optimizer,        
                    classifier_one_epoch, DEVICE,
-                   num_epochs=10000, callbacks=[es])
+                   num_epochs=10000, callbacks=[es],
+                   scheduler=scheduler,)
 
     torch.save(model.state_dict(), f"{MODEL_NAME}.pth")
     print("Model saved!")
